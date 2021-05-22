543b1ecb12287212a5ff6658b2c9b81009e42119
Vinicius Monteiro <dev.viniciusmonteiro@gmail.com>
2021-05-22 17:51:10 -0300

    Acertando o metodo get

--- 3/rotas/index.js
+++ 3/rotas/index.js
@@ -2,12 +2,9 @@ const roteador = require('express').Router()
 const tabelaAdress = require('./TabelaAddress')
 
 roteador.use('/',async (req,res)=>{//listar
-    //console.log("foi")
     const resultados = await tabelaAdress.listar()
     res.status(200)
-    
-    res.send(
-        
+    res.send( 
         JSON.stringify(resultados)
     )
 })
